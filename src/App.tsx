16ce99b06611141f4e10bffb6f5c8a6cf289cd2f
MariaEvdokimova <e.marrri@mail.ru>
2022-02-08 11:53:24 +0300

    Добавлено продолжение воспроизведения видео после перехода

--- src/App.tsx
+++ src/App.tsx
@@ -2,20 +2,25 @@ import React from 'react';
 import './App.css';
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
-import {Video} from "./components/Video/Video";
 import {PhoneAdd} from "./components/PhoneAdd/PhoneAdd";
 import {Route, Routes } from 'react-router-dom';
 import {FinalScreen} from "./components/FinalScreen/FinalScreen";
+import { Video } from './components/Video/Video';
+import { Context } from './Context';
 
 export const App: React.FC = () => {
 
+    const [context, setContext] = React.useState(0);
+
   return (
       <div className='app__wrapper'>
+          <Context.Provider value={[context, setContext]}>
           <Routes >
-              <Route path='/' element={<Video />} />
+              <Route path='/' element={<Video youtubeID='M7FIvfx5J10'/>} />
               <Route path='/phone' element={<PhoneAdd />}/>
               <Route path='/final' element={<FinalScreen />}/>
           </Routes >
+          </Context.Provider>
       </div>
   );
 }
--- src/components/Video/Video.tsx
+++ src/components/Video/Video.tsx
@@ -1,12 +1,30 @@
-import React, {useState} from 'react';
+import React, {useEffect, useState} from 'react';
 import YouTube from 'react-youtube';
 import classes from "./Video.module.css";
 import {Navigation} from "../Navigation/Navigation";
 import {Link} from "react-router-dom";
+import { Context } from '../../Context';
 
-export const Video: React.FC = () => {
-    const [youtubeID] = useState('M7FIvfx5J10');
+type PropsType = {
+    youtubeID: string
+}
+
+export const Video: React.FC<PropsType> = ({youtubeID}) => {
     const [IsBannerOpen, EditIsBannerOpen] = useState(false);
+    const [context, setContext] = React.useContext<any>(Context);
+
+    const [videoTime, editVideoTime] = useState(0);
+
+    const initialize = (e: any) => {
+        updateTimerDisplay(e.target);
+        setInterval(function () {
+            updateTimerDisplay(e.target);
+        }, 1000)
+    }
+
+    const updateTimerDisplay = (player: any) => {
+        setContext(player.getCurrentTime())
+    }
 
     const videoOnPlay = () => {
         if (!IsBannerOpen) {
@@ -14,21 +32,26 @@ export const Video: React.FC = () => {
         }
     }
 
+    useEffect(()=> {
+        editVideoTime(context)
+    },[])
+
     return (
         <div>
             <YouTube className={classes.video} videoId={youtubeID}
-                     opts={
-                         {
-                             height: '1280',
-                             width: '720',
-                             playerVars: {
-                                 'autoplay': 1,
-                                 'controls': 0,
-                                 origin: 'http://localhost:3000/'
-                            },
-                         }
+                 opts={
+                     {
+                         height: '1280',
+                         width: '720',
+                         playerVars: {
+                             'autoplay': 1,
+                             start: videoTime,
+                        },
                      }
-                     onPlay={ videoOnPlay } />
+                 }
+                 onPlay={ videoOnPlay }
+                 onReady={initialize}
+            />
             <Link className={classes.banner + ' ' + (IsBannerOpen ? classes.banner__open : '')} to='/phone'>Banner</Link>
             <Navigation linkPrev='/final' linkNext='/phone'/>
         </div>
